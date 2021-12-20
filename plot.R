41e8420563755526c750774f58b6ef90882dcd60
Peter Menzel <pmenzel@gmail.com>
2021-12-20 22:31:00 +0100

    Initial commit

---
+++ plot.R
@@ -0,0 +1,109 @@
+# inspiration: from
+# https://twitter.com/VictimOfMaths/status/1472968769353965568
+# https://github.com/VictimOfMaths/COVID-19/blob/master/Heatmaps/COVIDCasesLTLAPhasePlot.R
+
+library(tibble)
+library(readr)
+library(forcats)
+library(tidyr)
+library(stringr)
+library(magrittr)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+library(tidytext)
+library(ggforce)
+library(ggrepel)
+library(scales)
+library(reshape2)
+library(patchwork)
+library(plotly)
+library(lubridate)
+library(xlsx)
+library(curl)
+
+filename <- paste0(today(), "_Fallzahlen_Inzidenz.xlsx")
+urlRKI <- "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Inzidenz_aktualisiert.xlsx?__blob=publicationFile"
+curl_download(url = urlRKI, destfile = filename, quiet = FALSE, mode = "wb")
+
+DateOfInterest <- today() - days(1)
+
+# Bundesländer ----------------------------------------------------------------------------------------------------------------------------------------------
+
+df.raw.BL <- xlsx::read.xlsx(filename, sheetIndex = 5, stringsAsFactors = F, header = T, startRow = 2)
+
+df.long <- df.raw.BL %>%
+  select(-`NA.`) %>%
+  pivot_longer(-MeldeLandkreisBundesland, names_to = "Datum", values_to = "Inzidenz")
+
+df.plot <- df.long %>%
+  mutate(Datum = readr::parse_date(Datum, "X%d.%m.%Y")) %>%
+  transmute(BL = MeldeLandkreisBundesland, Datum, Inzidenz) %>%
+  group_by(BL) %>%
+    arrange(Datum) %>%
+    mutate(Inzidenz7DaysAgo = lag(Inzidenz, 7)) %>%
+    mutate(Change = Inzidenz - Inzidenz7DaysAgo) %>%
+  ungroup() %>%
+  filter(Datum >= DateOfInterest - days(7)) %>%
+  mutate(DaysSince = as.integer(DateOfInterest - Datum)) %>%
+  mutate(Highlight = ifelse(BL=="Gesamt", "y", "n"))
+
+p.BL <- df.plot %>%
+  ggplot() +
+  geom_hline(yintercept = 0, color = "grey20") +
+  geom_vline(xintercept = 0, color = "grey20") +
+  geom_path(aes(x = Inzidenz, y = Change, group = BL, alpha = 7 - DaysSince), color = "grey50", show.legend = FALSE) +
+  geom_point(data = df.plot %>% filter(Datum == today()), aes(x = Inzidenz, y = Change, fill = Highlight), shape = 21, size = 3) +
+  geom_text_repel(data = df.plot %>% filter(Datum == today()), aes(x = Inzidenz, y = Change, label = BL), color = "grey40") +
+  scale_x_continuous(name="7-Tage-Inzidenz (Fälle in der letzten Woche je 100k Einwohner)") +
+  scale_fill_manual(values = c("y" = "red", "n" = "grey50"), guide = "none") +
+  scale_y_continuous(name="Differenz zur 7-Tage-Inzidenz vor einer Woche") +
+  theme_bw() +
+  theme(axis.line = element_blank(), axis.ticks = element_blank()) +
+  labs(title = "Entwicklung 7-Tage-Inzidenzen der Bundesländer",
+       subtitle = paste0("Datum: ", DateOfInterest),
+       caption = "Daten von https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Inzidenz-Tabellen.html")
+
+ggsave(p.BL, filename = paste0(DateOfInterest, "_Entwicklung_Inzidenz_BL.png"), width = 10)
+
+
+# Landkreise ----------------------------------------------------------------------------------------------------------------------------------------------
+
+df.raw.LK <- xlsx::read.xlsx(filename, sheetIndex = 7, stringsAsFactors = F, header = T, startRow = 2)
+
+DateOfInterest <- today() - days(1)
+
+df.long <- df.raw.LK %>%
+  select(-`NA.`) %>%
+  filter(!is.na(MeldeLandkreis)) %>%
+  pivot_longer(cols = !c("IdMeldeLandkreis", "MeldeLandkreis"), names_to = "Datum", values_to = "Inzidenz")
+
+df.plot <- df.long %>%
+  mutate(Datum = readr::parse_date(Datum, "X%d.%m.%Y")) %>%
+  transmute(LK = MeldeLandkreis, Datum, Inzidenz) %>%
+  group_by(LK) %>%
+    arrange(Datum) %>%
+    mutate(Inzidenz7DaysAgo = lag(Inzidenz, 7)) %>%
+    mutate(Change = Inzidenz - Inzidenz7DaysAgo) %>%
+  ungroup() %>%
+  filter(Datum >= DateOfInterest - days(7)) %>%
+  mutate(DaysSince = as.integer(DateOfInterest - Datum))
+
+
+p.LK <- df.plot %>%
+  ggplot() +
+  geom_hline(yintercept = 0, color = "grey20") +
+  geom_vline(xintercept = 0, color = "grey20") +
+  geom_path(aes(x = Inzidenz, y = Change, group = LK, alpha = 7 - DaysSince), color = "grey50", show.legend = FALSE) +
+  geom_point(data = df.plot %>% filter(Datum == today()), aes(x = Inzidenz, y = Change), color = "deepskyblue4", alpha = 0.8) +
+  geom_text_repel(data = df.plot %>% filter(Datum == today()), aes(x = Inzidenz, y = Change, label = LK), size = 2.3) +
+  scale_x_continuous(name="7-Tage-Inzidenz (Fälle in der letzten Woche je 100k Einwohner)") +
+  scale_y_continuous(name="Differenz zur 7-Tage-Inzidenz vor einer Woche") +
+  theme_bw() +
+  theme(axis.line = element_blank(), axis.ticks = element_blank()) +
+  labs(title = "Entwicklung 7-Tage-Inzidenzen der Landkreise",
+       subtitle = paste0("Datum: ", DateOfInterest),
+       caption = "Daten von https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Inzidenz-Tabellen.html")
+
+ggsave(p.LK, filename = paste0(DateOfInterest, "_Entwicklung_Inzidenz_LK.png"), width = 10)
+
